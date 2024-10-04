b0e8eb16552a83423db24bbb0fa6db1bd6d2b82f
Segolene GANZIN <segolene.ganzin@gmail.com>
2024-10-04 09:56:56 +0200

    update sitter page

--- src/pages/Sitter.tsx
+++ src/pages/Sitter.tsx
@@ -65,7 +65,7 @@ const Sitter = () => {
                     <Cta
                       handleClick={() => navigate('/sign-in')}
                       classname='btn sitters-hero__cta'
-                      content='Connection'
+                      content='Connexion'
                     />
                     <SignLink
                       text={"Vous n'avez pas encore de compte ?"}
